e1c92176dcb7be88271897ce885e5f7cc9aea68e
guitarster <guitarster@posteo.de>
2025-07-29 13:20:29 +0200

    Add API-Request for current weather

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
+const currentWeatherEl = document.querySelector(".current-weather");
 
+async function getCurrentWeather() {
+  const url =
+    "http://api.weatherapi.com/v1/current.json?key=81d83f0595614a6e8dc93225252907&q=Dunningen&lang=de";
+
+  const response = await fetch(url);
+
+  const responseJSON = await response.json();
+
+  return responseJSON;
+}
+
+async function getForecastWeather() {
+  const url =
+    "http://api.weatherapi.com/v1//forecast.json?key=81d83f0595614a6e8dc93225252907&q=Dunningen&lang=de";
+
+  const response = await fetch(url);
+
+  const responseJSON = await response.json();
+
+  return responseJSON;
+}
+
+const currentWeather = await getCurrentWeather();
+const forecastWeather = await getForecastWeather();
+
+currentWeatherEl.innerHTML = `
+        <div class="current-weather__location">${currentWeather.location.name}</div>
+        <div class="current-weather__temperature">${currentWeather.current.temp_c}°</div>
+        <div class="current-weather__condition">${currentWeather.current.condition.text}</div>
+        <div class="current-weather__max-and-min-temperature">
+          <div class="curent-weather__max-temperature">${forecastWeather.forecast.forecastday[0].day.maxtemp_c}°</div>
+          <div class="curent-weather__min-temperature">${forecastWeather.forecast.forecastday[0].day.mintemp_c}°</div>
+        </div>`;
