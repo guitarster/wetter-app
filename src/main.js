9a716811392eb85bc8fa536176614821e8a20a54
guitarster <guitarster@posteo.de>
2025-07-29 14:09:31 +0200

    Add Spinner

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { getCurrentWeather, getForecastWeather } from "./API.js";
 
 const currentWeatherEl = document.querySelector(".current-weather");
+const spinnerEl = document.querySelector(".lds-ring");
+const appEl = document.querySelector(".app-default");
 
 const currentWeather = await getCurrentWeather();
 const forecastWeather = await getForecastWeather();
+
+if (currentWeather) {
+  spinnerEl.classList.remove("lds-ring");
+  appEl.classList.remove("app-default");
+  appEl.classList.add("app");
+}
 
 currentWeatherEl.innerHTML = `
         <div class="current-weather__location">${currentWeather.location.name}</div>
